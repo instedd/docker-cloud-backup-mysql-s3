e23f356dd291829ad10bf70815352f890d566a23
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-04-28 15:48:17 -0300

    Refactor to support other database types

--- backup.py
+++ backup.py
@@ -3,43 +3,41 @@ import dockercloud
 import os
 from datetime import datetime
 
-def service_envvar(service, varname):
-  for var in service.calculated_envvars:
-    if var['key'] == varname:
-      return var['value']
+class DbContainer:
+  def __init__(self, container):
+    self.container = container
+    self.service = dockercloud.Utils.fetch_by_resource_uri(container.service)
+    self.stack_name = self.service_envvar('DOCKERCLOUD_STACK_NAME') or "Unknown"
 
-  return None
+  def service_envvar(self, varname):
+    for var in self.service.calculated_envvars:
+      if var['key'] == varname:
+        return var['value']
 
-def mysql_password(service):
-  pwd = service_envvar(service, 'MYSQL_ROOT_PASSWORD')
-  if pwd == None:
-    if service_envvar(service, 'MYSQL_ALLOW_EMPTY_PASSWORD') == 'yes':
-      return ""
-    else:
-      return None
-  else:
-    return pwd
+    return None
 
-s3_bucket = os.getenv('S3_BUCKET')
-containers = dockercloud.Container.list()
+  def run_and_upload(self, dbname, cmd, s3_bucket):
+    print("Backing up {0}/{1}".format(container.name, dbname))
+    filename = "{0}-{1}.sql.gz".format(dbname, datetime.now().isoformat())
+    backup_and_upload_cmd = "{0} | gzip | aws s3 cp - s3://{1}/{2}/{3}/{4}".format(cmd, s3_bucket, self.stack_name, self.service.name, filename)
+    os.system(backup_and_upload_cmd)
 
-for container in containers:
-  if container.image_name.startswith('mysql:') and container.state == 'Running':
-    service = dockercloud.Utils.fetch_by_resource_uri(container.service)
-    stack_name = service_envvar(service, 'DOCKERCLOUD_STACK_NAME') or "Unknown"
 
+class MysqlContainer(DbContainer):
+  def backup(self, s3_bucket):
+    print("Backing up databases in MySQL container '{0}/{1}'".format(self.stack_name, self.container.name))
     # Determine MySQL root password
-    root_password = mysql_password(service)
+    root_password = self.mysql_password()
     if root_password == None:
-      print("Could not determine MySQL password for container {0}".format(container.name))
-      continue
+      print("Could not determine MySQL password for container '{0}/{1}'".format(self.stack_name, self.container.name))
+      return
     if root_password == '':
       password_arg = ''
     else:
       password_arg = "-p{0}".format(root_password)
 
     # Enumerate databases to backup
-    cmd = "mysql -u root {0} -h {1} -s --skip-column-names -e 'show databases'".format(password_arg, container.private_ip)
+    cmd = "mysql -u root {0} -h {1} -s --skip-column-names -e 'show databases'".format(password_arg, self.container.private_ip)
     dbs_output = os.popen(cmd).read()
 
     for db in dbs_output.splitlines():
@@ -48,8 +46,24 @@ for container in containers:
         continue
 
       # Execute backup, compress and upload to S3
-      print("Backing up {0}/{1}".format(container.name, db))
-      backup_cmd = "mysqldump -u root {0} -h {1} --single-transaction {2}".format(password_arg, container.private_ip, db)
-      filename = "{0}-{1}.sql.gz".format(db, datetime.now().isoformat())
-      backup_and_upload_cmd = "{0} | gzip | aws s3 cp - s3://{1}/{2}/{3}/{4}".format(backup_cmd, s3_bucket, stack_name, service.name, filename)
-      os.system(backup_and_upload_cmd)
+      backup_cmd = "mysqldump -u root {0} -h {1} --single-transaction {2}".format(password_arg, self.container.private_ip, db)
+      self.run_and_upload(db, backup_cmd, s3_bucket)
+
+  def mysql_password(self):
+    pwd = self.service_envvar('MYSQL_ROOT_PASSWORD')
+    if pwd == None:
+      if self.service_envvar('MYSQL_ALLOW_EMPTY_PASSWORD') == 'yes':
+        return ""
+      else:
+        return None
+    else:
+      return pwd
+
+s3_bucket = os.getenv('S3_BUCKET')
+for container in dockercloud.Container.list():
+  if container.state != 'Running':
+    continue
+
+  if container.image_name.startswith('mysql:'):
+    MysqlContainer(container).backup(s3_bucket)
+
